b5119844e79cbc95c8d1977ace85aa8f7db9ac9e
Muhammad Bilal <billu97.memon@gmail.com>
2020-08-17 00:29:24 +0500

    Added Redux DevTool

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
   // })
 
   store.dispatch(bugAdded("Bug 1"))
+  store.dispatch(bugAdded("Bug 2"))
+  store.dispatch(bugAdded("Bug 3"))
   store.dispatch(bugResolved(1))
   // unSubscribe();
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux';
 import reducer from '../reducers/reducers'
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 //  its also the higher order function
-const store = createStore(reducer);
+const store = createStore(reducer, devToolsEnhancer({ trace: true }));
 
 export default store;
